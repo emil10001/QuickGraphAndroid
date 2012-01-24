3295d76dfdf30078f11614fdb2442f705932948f
E. John Feig <e.john.feig@gmail.com>
2012-01-23 22:26:10 -0500

    first commit

---
+++ src/com/feigdev/quickgraphandroid/QuickGraphAndroidActivity.java
@@ -0,0 +1,41 @@
+package com.feigdev.quickgraphandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class QuickGraphAndroidActivity extends Activity {
+	WebView mWebView;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("file:///android_asset/Quick-Grapher/html/page1.html");
+        mWebView.setWebViewClient(new HelloWebViewClient());
+    }
+    
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    
+}
